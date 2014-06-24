d97a5a5316818742f910218f362da86e4be7cbf3
Alice Borie <aborie@andrew.cmu.edu>
2014-06-24 15:33:54 -0400

    Added scopes in user model for custom querying. Tested all scopes

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,43 @@ class User < ActiveRecord::Base
 	validates :user_visit_count, numericality: { greater_than_or_equal_to: 0, only_integer: true } 
 
 	# scopes 
+	# returns users who used AppBuilder in the past month
+	scope :last_month, -> { where('user_day_since_last >= ?', 1.month.ago) }
+	# returns users who used AppBuilder in the last 6 months
+	scope :last_6_months, -> { where('user_day_since_last >= ?', 6.month.ago) }
+	# returns users who used AppBuilder in the past year 
+	scope :last_year, -> { where('user_day_since_last >= ?', 1.year.ago) }
+	# returns users who used AppBuilder in between date1 and date2
+	scope :used_between, ->(date1, date2) { where('user_day_since_last >= ? and user_day_since_last <= ?', date1, date2)}
+	# returns old users since date
+	scope :old_users_since, ->(date) { where('user_day_since_first <= ?', date) }
+	# returns new users since
+	scope :new_users_since, ->(date) { where('user_day_since_first >= ?', date) }
+	# returns new users between date1 and date2
+	scope :new_users_between, ->(date1, date2) { where('user_day_since_first >= ? and user_day_since_first <= ?', date1, date2)}
+	# returns users arranged by visit count (descending, high to low)
+	# manually tested, no test in user_test
+	scope :by_visit_count_d, -> { order('user_visit_count DESC') }
+	# returns users arranged by visit count (ascneding, low to high, ascending)
+	# manually tested, no test in user_test
+	scope :by_visit_count_a, -> { order('user_visit_count ASC') }
+	# returns users between lower bound and upper bound visit counts (inclusive)
+	scope :visit_count_between, ->(l_bound, u_bound) { where('user_visit_count >= ? and user_visit_count <= ?', l_bound, u_bound)}
+	# returns returning users 
+	scope :returning_users, -> { where(user_returning: true) }
+	# returns new users 
+	scope :new_users, -> { where(user_returning: false) }
+	# returns users ordered by last time used 
+	# manually tested, no test in user_test
+	scope :recent_users, -> { order('user_day_since_last DESC') }
+	# returns users ordered by last time used 
+	# manually tested, no test in user_test
+	scope :oldest_users, -> { order('user_day_since_last ASC') }
 
 	# methods 
+	# # returns amount of time between first and last minute
+	# def user_for
+	# end
 
 	# callbacks 
 
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -27,5 +27,45 @@ class UserTest < ActiveSupport::TestCase
       assert_equal 10, User.all.size
     end
 
+    should "show that there is one user who used AppBuilder in the last month" do
+      assert_equal 1, User.last_month.size
+    end
+
+    should "show that there are three users who used AppBuilder in the last 6 months" do
+      assert_equal 3, User.last_6_months.size
+    end
+
+    should "show that there are seven users who used AppBuilder in the last 7 years" do
+      assert_equal 7, User.last_year.size
+    end
+
+    should "show that there two users who last used AppBuilder at one point between August 15, 2013 and September 17, 2013" do
+      assert_equal 2, User.used_between(Date.new(2013,8,15), Date.new(2013,9,17)).size
+    end
+
+    should "show that there are 7 old users since May 11, 2012" do
+      assert_equal 7, User.old_users_since(Date.new(2012,5,11)).size
+    end
+
+    should "show that there are 3 new users since May 11, 2012" do
+      assert_equal 3, User.new_users_since(Date.new(2012,5,11)).size
+    end
+
+    should "show that there is 1 new user from July 5, 2010 and July 8, 2010" do
+      assert_equal 1, User.new_users_between(Date.new(2010,7,5), Date.new(2010,7,8)).size
+    end
+
+    should "show there are 3 users with visit counts between 40 and 85" do
+      assert_equal 3, User.visit_count_between(40,85).size
+    end
+
+    should "show there are 10 returning users" do
+      assert_equal 10, User.returning_users.size
+    end
+
+    should "show there are 0 new users" do
+      assert_equal 0, User.new_users.size
+    end
+
   end
 end
